a14d7fe39c6e9f87bc83570b8cff20f4687115f7
Louis <herren.louis@gmx.ch>
2022-02-20 16:40:24 +0100

    DiaryEntry class added; added classes and dbcontext for infrastructre

---
+++ OOPGruppenArbeitLHOFLH/Infrastructure/EntryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGruppenArbeitLHOFLH.Infrastructure
+{
+    public class EntryDbContext : DbContext
+    {
+        public EntryDbContext() : base("name=OOPGruppenArbeitDb")
+        {
+            Database.ExecuteSqlCommand(
+                "CREATE TABLE IF NOT EXISTS 'DiaryEntries' ('Id' TEXT PRIMARY KEY, 'Description' TEXT, 'Status' INTEGER)");
+        }
+    }
+}
