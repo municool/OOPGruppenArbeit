da4c72fa1070bb13c648c5dd2a86d68935bf0cce
Louis <herren.louis@gmx.ch>
2022-02-20 15:51:06 +0100

    defined interfaces

---
+++ OOPGruppenArbeitLHOFLH/IBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGruppenArbeitLHOFLH
+{
+    public interface IBusiness
+    {
+        DiaryEntry GetDiaryEntry(DateTime date);
+
+        void SaveDiaryEntry(DiaryEntry entry);
+
+        List<string> GetAvailableTags();
+
+        bool IsValidEntry(DiaryEntry entry);
+
+    }
+}
---
+++ OOPGruppenArbeitLHOFLH/IInfrastructure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGruppenArbeitLHOFLH
+{
+    public interface IInfrastructure
+    {
+        void SaveDiaryEntry(DiaryEntry entry);
+        
+        DiaryEntry GetDiaryEntry(string key);
+
+        List<DiaryEntry> GetDiaryEntries(DateTime? start = null, DateTime? end = null);
+
+        List<string> GetTags();
+    }
+}
