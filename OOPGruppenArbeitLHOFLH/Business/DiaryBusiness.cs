c9c9f6ffdcf0748351d908818b51c0f01468e9ed
OliverFrey <58007575+OliverFrey@users.noreply.github.com>
2022-02-20 17:19:27 +0100

    Rename class Methode IsValidEntry

---
+++ OOPGruppenArbeitLHOFLH/Business/DiaryBusiness.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGruppenArbeitLHOFLH
+{
+
+    public class DiaryBusiness : IBusiness
+    {
+        public List<string> GetAvailableTags()
+        {
+            
+
+
+            throw new NotImplementedException();
+        }
+
+        public DiaryEntry GetDiaryEntry(DateTime date)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+
+        public bool IsValidEntry(DiaryEntry entry)
+        {
+            string dentry = entry.ToString();
+ 
+            if(dentry.Length <= 1000)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SaveDiaryEntry(DiaryEntry entry)
+        {
+            if()
+           
+
+        }
+    }
+}
