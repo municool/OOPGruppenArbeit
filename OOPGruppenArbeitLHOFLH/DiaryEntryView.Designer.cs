3eb0ef15355dc6dad76f2aae0cfe6184fd14d397
luckyluuc <lucahuegli@gmail.com>
2022-02-23 08:03:11 +0100

    Save_Click, DateDecrease and DateIncrease buttons fixed

--- OOPGruppenArbeitLHOFLH/DiaryEntryView.Designer.cs
+++ OOPGruppenArbeitLHOFLH/DiaryEntryView.Designer.cs
@@ -29,84 +29,117 @@
         private void InitializeComponent()
         {
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxDiaryInput = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.uploadButton = new System.Windows.Forms.Button();
+            this.textBoxTags = new System.Windows.Forms.TextBox();
+            this.UploadButton = new System.Windows.Forms.Button();
+            this.DateIncreaseButton = new System.Windows.Forms.Button();
+            this.DateDecreaseButton = new System.Windows.Forms.Button();
+            this.TagsLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Location = new System.Drawing.Point(38, 12);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
-            // textBox1
+            // textBoxDiaryInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
-            this.textBox1.MaxLength = 1000;
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(400, 265);
-            this.textBox1.TabIndex = 1;
+            this.textBoxDiaryInput.Location = new System.Drawing.Point(12, 38);
+            this.textBoxDiaryInput.MaxLength = 1000;
+            this.textBoxDiaryInput.Multiline = true;
+            this.textBoxDiaryInput.Name = "textBoxDiaryInput";
+            this.textBoxDiaryInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDiaryInput.Size = new System.Drawing.Size(400, 265);
+            this.textBoxDiaryInput.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(418, 38);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(198, 158);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 265);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(542, 308);
+            this.saveButton.Location = new System.Drawing.Point(743, 312);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 3;
-            this.saveButton.Text = "Save";
+            this.saveButton.Text = "Save Entry";
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.Save_Click);
             // 
-            // openFileDialog1
+            // textBoxTags
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            this.textBoxTags.Location = new System.Drawing.Point(52, 311);
+            this.textBoxTags.Name = "textBoxTags";
+            this.textBoxTags.Size = new System.Drawing.Size(198, 20);
+            this.textBoxTags.TabIndex = 4;
             // 
-            // textBox2
+            // UploadButton
             // 
-            this.textBox2.Location = new System.Drawing.Point(418, 231);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(198, 20);
-            this.textBox2.TabIndex = 4;
+            this.UploadButton.Location = new System.Drawing.Point(418, 311);
+            this.UploadButton.Name = "UploadButton";
+            this.UploadButton.Size = new System.Drawing.Size(198, 24);
+            this.UploadButton.TabIndex = 5;
+            this.UploadButton.Text = "Upload Picture";
+            this.UploadButton.UseVisualStyleBackColor = true;
+            this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
             // 
-            // uploadButton
+            // DateIncreaseButton
             // 
-            this.uploadButton.Location = new System.Drawing.Point(418, 201);
-            this.uploadButton.Name = "uploadButton";
-            this.uploadButton.Size = new System.Drawing.Size(198, 24);
-            this.uploadButton.TabIndex = 5;
-            this.uploadButton.Text = "Upload";
-            this.uploadButton.UseVisualStyleBackColor = true;
+            this.DateIncreaseButton.Location = new System.Drawing.Point(244, 12);
+            this.DateIncreaseButton.Name = "DateIncreaseButton";
+            this.DateIncreaseButton.Size = new System.Drawing.Size(20, 20);
+            this.DateIncreaseButton.TabIndex = 6;
+            this.DateIncreaseButton.Text = ">";
+            this.DateIncreaseButton.UseVisualStyleBackColor = true;
+            this.DateIncreaseButton.Click += new System.EventHandler(this.DateIncreaseButton_Click);
+            // 
+            // DateDecreaseButton
+            // 
+            this.DateDecreaseButton.Location = new System.Drawing.Point(12, 12);
+            this.DateDecreaseButton.Name = "DateDecreaseButton";
+            this.DateDecreaseButton.Size = new System.Drawing.Size(20, 20);
+            this.DateDecreaseButton.TabIndex = 7;
+            this.DateDecreaseButton.Text = "<";
+            this.DateDecreaseButton.UseVisualStyleBackColor = true;
+            this.DateDecreaseButton.Click += new System.EventHandler(this.DateDecreaseButton_Click);
+            // 
+            // TagsLabel
+            // 
+            this.TagsLabel.AutoSize = true;
+            this.TagsLabel.Location = new System.Drawing.Point(12, 314);
+            this.TagsLabel.Name = "TagsLabel";
+            this.TagsLabel.Size = new System.Drawing.Size(34, 13);
+            this.TagsLabel.TabIndex = 8;
+            this.TagsLabel.Text = "Tags:";
             // 
             // DiaryEntryView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 343);
-            this.Controls.Add(this.uploadButton);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(827, 343);
+            this.Controls.Add(this.TagsLabel);
+            this.Controls.Add(this.DateDecreaseButton);
+            this.Controls.Add(this.DateIncreaseButton);
+            this.Controls.Add(this.UploadButton);
+            this.Controls.Add(this.textBoxTags);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxDiaryInput);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "DiaryEntryView";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Diary";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,12 +149,15 @@
         #endregion
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        internal System.Windows.Forms.TextBox textBox1;
+        internal System.Windows.Forms.TextBox textBoxDiaryInput;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.TextBox textBoxTags;
+        private System.Windows.Forms.Button UploadButton;
+        private System.Windows.Forms.Button DateIncreaseButton;
+        private System.Windows.Forms.Button DateDecreaseButton;
+        private System.Windows.Forms.Label TagsLabel;
     }
 }
 
--- OOPGruppenArbeitLHOFLH/DiaryEntryView.cs
+++ OOPGruppenArbeitLHOFLH/DiaryEntryView.cs
@@ -8,24 +8,73 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace OOPGruppenArbeitLHOFLH
 {
     public partial class DiaryEntryView : Form
     {
+
+        IBusiness business = null;
+        DiaryEntry currentEntry = null;
+
         public DiaryEntryView()
         {
             InitializeComponent();
+            // business = 
+            // Daten abrufen (mit Methode)
         }
 
-        private void Save_Click(object sender, EventArgs e)
+        public void Save_Click(object sender, EventArgs e)
         {
+            if (currentEntry == null)
+            {
+                currentEntry = new DiaryEntry();
+            }
+
+            // Daten currentEntry zuweisen
+            currentEntry.DateTime = dateTimePicker1.Value;
+            currentEntry.EntryText = textBoxDiaryInput.Text;
+            currentEntry.Tags = textBoxTags.Text;
+            currentEntry.PicturePath = pictureBox1.ImageLocation;
+
             // Speichern-Methode aufrufen
+            business.SaveDiaryEntry(currentEntry);
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        public void UploadButton_Click(object sender, EventArgs e)
         {
-
+            // Dialogfenster zum Fileupload öffnen inkl. Filter
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    // Bild in pictureBox anzeigen
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
         }
+
+        private void DateDecreaseButton_Click(object sender, EventArgs e)
+        {
+            // DateTimePicker -1 Tag
+            dateTimePicker1.Value = dateTimePicker1.Value.AddDays(-1);
+        }
+
+        private void DateIncreaseButton_Click(object sender, EventArgs e)
+        {
+            // DateTimePicker +1 Tag
+            dateTimePicker1.Value = dateTimePicker1.Value.AddDays(1);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // Daten abrufen
+        }
+
+        // Methode zum Datenabruf
+
+        // Textbox Tags: Aufruf GetAvailableTags();
 
     }
 }
