a766231b93522e74e83f95d9292341edf0c7754c
OliverFrey <58007575+OliverFrey@users.noreply.github.com>
2022-02-26 11:51:33 +0100

    Funktionen GetDiaryEntry, IsValidEntry und SaveDiaryEntry

--- OOPGruppenArbeitLHOFLH/Business/DiaryBusiness.cs
+++ OOPGruppenArbeitLHOFLH/Business/DiaryBusiness.cs
@@ -9,9 +9,11 @@ namespace OOPGruppenArbeitLHOFLH
 
     public class DiaryBusiness : IBusiness
     {
+        IInfrastructure infrastructure = new DiaryInfrastructure();
+
         public List<string> GetAvailableTags()
         {
-            
+            // Aus IInfrastructure aufrufen
 
 
             throw new NotImplementedException();
@@ -19,29 +21,36 @@ namespace OOPGruppenArbeitLHOFLH
 
         public DiaryEntry GetDiaryEntry(DateTime date)
         {
-            
+            //Tagebucheintrag aufrufen
+            var diaryEntry = infrastructure.GetDiaryEntries(date, date).FirstOrDefault();
 
-            throw new NotImplementedException();
+            return diaryEntry;
         }
 
         public bool IsValidEntry(DiaryEntry entry)
         {
-            string dentry = entry.ToString();
- 
-            if(dentry.Length <= 1000)
-            {
-                return true;
-            } 
-            else
-            {
-                return false;
-            }
+            //entry auf Anzahl Zeichen prüfen
+            //Wenn 1000 überschritten werden, Fehler ausgeben
+            return entry.EntryText.Length <= 1000 && entry.GetTags().Count <= 3;
         }
 
         public void SaveDiaryEntry(DiaryEntry entry)
         {
-            if()
-           
+            //Methode IsValidEntry aufrufen
+            DiaryBusiness boolean = new DiaryBusiness();
+
+            bool result = boolean.IsValidEntry(entry); 
+
+            
+
+
+            //Methode SaveDiaryEntry  aus IInfrastructure ausführen
+            if (result == false)
+            {
+                throw new Exception("Der Text hat mehr als 1000 Zeichen oder mehr als 3 Tags.");
+            }
+
+            infrastructure.SaveDiaryEntry(entry);
 
         }
     }
