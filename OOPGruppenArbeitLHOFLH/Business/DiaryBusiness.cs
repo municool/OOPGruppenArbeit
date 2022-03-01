81d43d0115157c88ca77f19255568b2fe2d846c7
OliverFrey <58007575+OliverFrey@users.noreply.github.com>
2022-03-01 21:49:26 +0100

    Methode Login und Verschlüsselung

--- OOPGruppenArbeitLHOFLH/Business/DiaryBusiness.cs
+++ OOPGruppenArbeitLHOFLH/Business/DiaryBusiness.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace OOPGruppenArbeitLHOFLH
 {
@@ -20,37 +21,61 @@ namespace OOPGruppenArbeitLHOFLH
 
         public DiaryEntry GetDiaryEntry(DateTime date)
         {
-            //Tagebucheintrag aufrufen
             var diaryEntry = infrastructure.GetDiaryEntries(date, date).FirstOrDefault();
 
             return diaryEntry;
         }
 
-        public bool IsValidEntry(DiaryEntry entry)
+        private bool IsValidEntry(DiaryEntry entry)
         {
-            //entry auf Anzahl Zeichen prüfen
-            //Wenn 1000 überschritten werden, Fehler ausgeben
             return entry.EntryText.Length <= 1000 && entry.GetTags().Count <= 3;
         }
 
         public void SaveDiaryEntry(DiaryEntry entry)
         {
-            //Methode IsValidEntry aufrufen
             DiaryBusiness boolean = new DiaryBusiness();
 
             bool result = boolean.IsValidEntry(entry); 
 
-            
-
-
-            //Methode SaveDiaryEntry  aus IInfrastructure ausführen
             if (result == false)
             {
                 throw new Exception("Der Text hat mehr als 1000 Zeichen oder mehr als 3 Tags.");
             }
 
             infrastructure.SaveDiaryEntry(entry);
-
         }
+
+
+
+        public void Login(string username, string password) //Methodenname Inhalt muss abgestimmt werden
+        {
+            //Passwort verschlüsseln
+            string hashString = getHashSha256(password);
+
+            if(username == null && hashString == null)
+            {
+                //Methode zum aufrufen des Form
+            }
+            else
+            {
+                return "Your Login is not valid";
+            }
+
+          
+        }
+        public static string getHashSha256(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+
+        */
     }
 }
